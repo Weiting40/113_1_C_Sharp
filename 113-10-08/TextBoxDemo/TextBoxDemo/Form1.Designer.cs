ecdfaac12bbab3d76e6aff40ff0b3f7809d58d2b
Weiting40 <a113221040@mail.shu.edu.tw>
2024-10-08 18:04:57 +0800

    113-10-08

---
+++ 113-10-08/TextBoxDemo/TextBoxDemo/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TextBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            txtFirstName = new TextBox();
+            lblshow = new Label();
+            btnConfirm_Click = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft JhengHei UI", 24F);
+            label1.Location = new Point(59, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(189, 41);
+            label1.TabIndex = 0;
+            label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft JhengHei UI", 24F);
+            label2.Location = new Point(59, 194);
+            label2.Name = "label2";
+            label2.Size = new Size(186, 41);
+            label2.TabIndex = 1;
+            label2.Text = "Last Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft JhengHei UI", 24F);
+            label3.Location = new Point(59, 309);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 41);
+            label3.TabIndex = 2;
+            label3.Text = "Full Name:";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Microsoft JhengHei UI", 24F);
+            textBox2.Location = new Point(275, 187);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(215, 48);
+            textBox2.TabIndex = 25;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Font = new Font("Microsoft JhengHei UI", 24F);
+            txtFirstName.Location = new Point(275, 75);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(215, 48);
+            txtFirstName.TabIndex = 27;
+            // 
+            // lblshow
+            // 
+            lblshow.BorderStyle = BorderStyle.Fixed3D;
+            lblshow.Font = new Font("Microsoft JhengHei UI", 24F);
+            lblshow.Location = new Point(275, 294);
+            lblshow.Name = "lblshow";
+            lblshow.Size = new Size(215, 56);
+            lblshow.TabIndex = 28;
+            lblshow.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnConfirm_Click
+            // 
+            btnConfirm_Click.Location = new Point(261, 385);
+            btnConfirm_Click.Name = "btnConfirm_Click";
+            btnConfirm_Click.Size = new Size(122, 39);
+            btnConfirm_Click.TabIndex = 29;
+            btnConfirm_Click.Text = "Comfirm";
+            btnConfirm_Click.UseVisualStyleBackColor = true;
+            btnConfirm_Click.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnConfirm_Click);
+            Controls.Add(lblshow);
+            Controls.Add(txtFirstName);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox txtFirstName;
+        private Label lblshow;
+        private Button btnConfirm_Click;
+    }
+}
---
+++ 113-10-08/TextBoxDemo/TextBoxDemo/Form1.cs
@@ -0,0 +1,25 @@
+namespace TextBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fullName;
+            string firstName;
+            string lastName;
+
+            firstName = txtFirstName.Text;
+
+        }
+    }
+}
