56d4912def4eee4de2feabb8f339fb429f5c2d22
Weiting40 <a113221040@mail.shu.edu.tw>
2024-11-04 21:38:47 +0800

    113-11-04

---
+++ HW2/HW2/HW2/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace HW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label8 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(77, 76);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(296, 312);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tickets Sold";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Location = new Point(401, 76);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(330, 312);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Revenue Generated";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 115);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 26);
+            label1.TabIndex = 3;
+            label1.Text = "Class A:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 169);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 26);
+            label2.TabIndex = 4;
+            label2.Text = "Class B:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 233);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 26);
+            label3.TabIndex = 5;
+            label3.Text = "Class C:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(45, 67);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 26);
+            label4.TabIndex = 6;
+            label4.Text = "Class A:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(45, 129);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 26);
+            label5.TabIndex = 7;
+            label5.Text = "Class B:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(45, 181);
+            label6.Name = "label6";
+            label6.Size = new Size(87, 26);
+            label6.TabIndex = 8;
+            label6.Text = "Class C:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(45, 234);
+            label7.Name = "label7";
+            label7.Size = new Size(67, 26);
+            label7.TabIndex = 9;
+            label7.Text = "Total:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(125, 107);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(133, 34);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(125, 161);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(133, 34);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(125, 225);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(133, 34);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(147, 67);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(133, 34);
+            textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(147, 129);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(133, 34);
+            textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(147, 178);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(133, 34);
+            textBox6.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(147, 231);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(133, 34);
+            textBox7.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(123, 442);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 86);
+            button1.TabIndex = 2;
+            button1.Text = "Calculate Revenue";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(310, 442);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 86);
+            button2.TabIndex = 3;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(493, 442);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 86);
+            button3.TabIndex = 4;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.AutoEllipsis = true;
+            label8.Font = new Font("Microsoft JhengHei UI", 6.857143F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label8.Location = new Point(26, 48);
+            label8.Name = "label8";
+            label8.Size = new Size(252, 53);
+            label8.TabIndex = 5;
+            label8.Text = "Enter the number of tickets sold for each class of seats.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 26F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(768, 556);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private Label label7;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label8;
+    }
+}
