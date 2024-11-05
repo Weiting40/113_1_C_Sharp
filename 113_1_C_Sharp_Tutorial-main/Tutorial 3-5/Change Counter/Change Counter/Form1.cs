d99c69a6ebd761f361bcffd59f1f96383faa4fc3
Weiting40 <a113221040@mail.shu.edu.tw>
2024-11-05 17:55:59 +0800

    113-11-05

--- 113_1_C_Sharp_Tutorial-main/Tutorial 3-5/Change Counter/Change Counter/Form1.cs
+++ 113_1_C_Sharp_Tutorial-main/Tutorial 3-5/Change Counter/Change Counter/Form1.cs
@@ -12,51 +12,12 @@ namespace Change_Counter
 {
     public partial class Form1 : Form
     {
-        const decimal FIVE_CENTS_VALUE = 0.05m;
-        const decimal TEN_CENTS_VALUE = 0.10m;
-        const decimal TWENTY_FIVE_CENTS_VALUE = 0.25m;
-        const decimal FIFTY_CENTS_VALUE = 0.50M;
-
-        private decimal total=0m;
+       
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
-        {
-            // total = total + 0.05m;
-            total += FIVE_CENTS_VALUE;
-            totalLabel.Text = total.ToString("c");
-        }
-            
-        private void tenCentsPictureBox_Click(object sender, EventArgs e)
-        {
-            total += TEN_CENTS_VALUE;
-            totalLabel.Text = total.ToString("c");
-        }
-
-        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
-        {
-            total += TWENTY_FIVE_CENTS_VALUE;
-            totalLabel.Text = total.ToString("c");
-        }
-
-        private void fiftyCentsPictureBox_Click(object sender, EventArgs e)
-        {
-            total += FIFTY_CENTS_VALUE;
-            totalLabel.Text = total.ToString("c");
-        }
-
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnReset_Click(object sender, EventArgs e)
-        {
-            total = 0m;
-            totalLabel.Text = total.ToString("c");
-        }
+        
     }
 }
---
+++ HW-2/HW-2/HW-2/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace HW_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label8 = new Label();
+            classCtextBox = new TextBox();
+            classBtextBox = new TextBox();
+            classAtextBox = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            totallbl = new Label();
+            classClbl = new Label();
+            classBlbl = new Label();
+            classAlbl = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(classCtextBox);
+            groupBox1.Controls.Add(classBtextBox);
+            groupBox1.Controls.Add(classAtextBox);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(26, 16);
+            groupBox1.Margin = new Padding(2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(2);
+            groupBox1.Size = new Size(196, 166);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Tickets Sold";
+            // 
+            // label8
+            // 
+            label8.Font = new Font("Microsoft JhengHei UI", 7F);
+            label8.Location = new Point(18, 29);
+            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(166, 27);
+            label8.TabIndex = 5;
+            label8.Text = "Enter the number of tickets sold for each class of seats.";
+            // 
+            // classCtextBox
+            // 
+            classCtextBox.Location = new Point(72, 126);
+            classCtextBox.Margin = new Padding(2);
+            classCtextBox.Name = "classCtextBox";
+            classCtextBox.Size = new Size(113, 23);
+            classCtextBox.TabIndex = 4;
+            // 
+            // classBtextBox
+            // 
+            classBtextBox.Location = new Point(72, 97);
+            classBtextBox.Margin = new Padding(2);
+            classBtextBox.Name = "classBtextBox";
+            classBtextBox.Size = new Size(113, 23);
+            classBtextBox.TabIndex = 3;
+            // 
+            // classAtextBox
+            // 
+            classAtextBox.Location = new Point(72, 68);
+            classAtextBox.Margin = new Padding(2);
+            classAtextBox.Name = "classAtextBox";
+            classAtextBox.Size = new Size(113, 23);
+            classAtextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 126);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Class C:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 97);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Class B:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 68);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Class A:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(totallbl);
+            groupBox2.Controls.Add(classClbl);
+            groupBox2.Controls.Add(classBlbl);
+            groupBox2.Controls.Add(classAlbl);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Location = new Point(261, 21);
+            groupBox2.Margin = new Padding(2);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(2);
+            groupBox2.Size = new Size(232, 156);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Revenue Generated";
+            // 
+            // totallbl
+            // 
+            totallbl.BorderStyle = BorderStyle.Fixed3D;
+            totallbl.Location = new Point(77, 113);
+            totallbl.Margin = new Padding(2, 0, 2, 0);
+            totallbl.Name = "totallbl";
+            totallbl.Size = new Size(124, 23);
+            totallbl.TabIndex = 9;
+            // 
+            // classClbl
+            // 
+            classClbl.BorderStyle = BorderStyle.Fixed3D;
+            classClbl.Location = new Point(77, 84);
+            classClbl.Margin = new Padding(2, 0, 2, 0);
+            classClbl.Name = "classClbl";
+            classClbl.Size = new Size(124, 23);
+            classClbl.TabIndex = 8;
+            // 
+            // classBlbl
+            // 
+            classBlbl.BorderStyle = BorderStyle.Fixed3D;
+            classBlbl.Location = new Point(77, 60);
+            classBlbl.Margin = new Padding(2, 0, 2, 0);
+            classBlbl.Name = "classBlbl";
+            classBlbl.Size = new Size(124, 18);
+            classBlbl.TabIndex = 7;
+            // 
+            // classAlbl
+            // 
+            classAlbl.BorderStyle = BorderStyle.Fixed3D;
+            classAlbl.Location = new Point(77, 33);
+            classAlbl.Margin = new Padding(2, 0, 2, 0);
+            classAlbl.Name = "classAlbl";
+            classAlbl.Size = new Size(124, 18);
+            classAlbl.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(9, 33);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Class A:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(9, 63);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(48, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Class B:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 92);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(49, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Class C:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 121);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(39, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Total:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 227);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 54);
+            button1.TabIndex = 2;
+            button1.Text = "Calculate Revenue";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(184, 227);
+            button2.Margin = new Padding(2);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 54);
+            button2.TabIndex = 3;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += this.button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(325, 227);
+            button3.Margin = new Padding(2);
+            button3.Name = "button3";
+            button3.Size = new Size(88, 54);
+            button3.TabIndex = 4;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += this.button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(533, 374);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Stadium Seating";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox classCtextBox;
+        private TextBox classBtextBox;
+        private TextBox classAtextBox;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label8;
+        private Label totallbl;
+        private Label classClbl;
+        private Label classBlbl;
+        private Label classAlbl;
+    }
+}
